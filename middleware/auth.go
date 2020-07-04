c7c7e8a46f6252162af13ef425318cf66321a12d
leikewei <leikewei@theduapp.com>
2020-07-04 15:02:53 +0800

    add user auth

---
+++ middleware/auth.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"github.com/gogf/gf/net/ghttp"
+	"github/pibigstar/go-gateway/app/response"
+	"github/pibigstar/go-gateway/utils/token"
+)
+
+// 用户权限校验
+func Auth() func(r *ghttp.Request) {
+	return func(r *ghttp.Request) {
+		_, err := token.GetUserInfoFromSession(r)
+		if err != nil {
+			response.Error(r, err)
+			return
+		}
+		r.Middleware.Next()
+	}
+}
